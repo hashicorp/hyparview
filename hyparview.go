9a2a377468ea109ac0ed5d9ea71c0a5272708083
Lang Martin <lang@hashicorp.com>
2019-11-18 15:04:19 -0500

    RecvForwardJoin

--- hyparview.go
+++ hyparview.go
@@ -10,7 +10,16 @@ type Hyparview struct {
 
 type Node string
 
-func (v *Hyparview) ReceiveJoin(node Node) (ms []Message) {
+func (v *Hyparview) Recv(message *Message) {
+	switch Message.Command {
+	case Join:
+		ms := RecvJoin(Message.From)
+	case ForwardJoin:
+		ms := RecvForwardJoin(Message.Data, Message.TTL, Message.From)
+	}
+}
+
+func (v *Hyparview) RecvJoin(node Node) (ms []Message) {
 	if v.Active.IsFull() {
 		v.Active.DropRando()
 	}
@@ -23,6 +32,23 @@ func (v *Hyparview) ReceiveJoin(node Node) (ms []Message) {
 		}
 		ms = append(ms, SendForwardJoin(n, node, v.ActiveRWL, v.Self))
 	}
+	return ms
+}
 
+func (v *Hyparview) RecvForwardJoin(node Node, ttl int, sender Node) (ms []Message) {
+	if ttl == 0 || len(v.Active) == 0 {
+		v.Active.Add(node)
+	} else {
+		if ttl == v.PassiveRWL {
+			v.Passive.Add(node)
+		}
+	}
+
+	for _, n := range v.Active.Nodes {
+		if n.Equal(sender) {
+			continue
+		}
+		ms = append(ms, SendForwardJoin(n, node, ttl-1, v.Self))
+	}
 	return ms
 }
