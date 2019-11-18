c94e46a5d6dfc9a7f3144ed06c938977bc3bf504
Lang Martin <lang@hashicorp.com>
2019-11-18 16:35:45 -0500

    add default object creation

--- hyparview.go
+++ hyparview.go
@@ -1,14 +1,37 @@
 package hyparview
 
-type Hyparview struct {
-	Active     ActiveView
-	ActiveRWL  int
-	Passive    PassiveView
-	PassiveRWL int
-	Self       Node
+type Config struct {
+	ActiveSize  int
+	ActiveRWL   int
+	PassiveSize int
+	PassiveRWL  int
+	CryptoRand  bool
 }
 
-type Node string
+type Hyparview struct {
+	Config
+	Active  ActiveView
+	Passive PassiveView
+	Self    Node
+}
+
+func CreateView(self Node, active int, passive int, activeRWL int, passiveRWL int) *Hyparview {
+	return Hyparview{
+		Config: Config{
+			ActiveRWL:   activeRWL,
+			ActiveSize:  active,
+			PassiveRWL:  activeRWL,
+			PassiveSize: passive,
+		},
+		Active:  Active{Nodes: []Node{}, Max: active},
+		Passive: Active{Nodes: []Node{}, Max: active},
+		Self:    self,
+	}
+}
+
+func DefaultView(self Node, n size) *Hyparview {
+	return CreateView(self, 5, 30, 7, 5)
+}
 
 func (v *Hyparview) Recv(message *Message) {
 	switch Message.Command {
@@ -51,4 +74,12 @@ func (v *Hyparview) RecvForwardJoin(node Node, ttl int, sender Node) (ms []Messa
 		ms = append(ms, SendForwardJoin(n, node, ttl-1, v.Self))
 	}
 	return ms
+}
+
+func (v *Hyparview) DropRandActive() []Message {
+	ns := v.Active.Nodes
+	mx := len(v.Active.Nodes) - 1
+	i := v.rint(mx)
+	ns = append(ns[0:i], ns[i+1:mx])
+	v.Active.Nodes = ms
 }
