c19a7ca240803594381524bdd1cf0fec8240cb2c
Lang Martin <lang@hashicorp.com>
2019-11-18 16:36:10 -0500

    util: move rint out

---
+++ util.go
@@ -0,0 +1,20 @@
+package hyparview
+
+import (
+	cr "crypto/rand"
+	"math/big"
+	"math/rand"
+)
+
+// rint returns a random integer, but allows to choose at configure time to use the pseudo
+// random or cryptographic random number generator
+func (v *Hyparview) rint(n int) int {
+	if c.CryptoRand {
+		bn := new(big.Int).SetInt64(int64(n))
+		bi, _ := cr.Int(cr.Reader, bn)
+		i := int(bi.Int64())
+		return i
+	}
+	// math/rand
+	return rand.Intn(n)
+}
