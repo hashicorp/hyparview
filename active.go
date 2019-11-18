cabc6d233ead9c5924fe5727fce678210baa5bdf
Lang Martin <lang@hashicorp.com>
2019-11-18 18:24:46 -0500

    active DelIndex & GetIndex

--- active.go
+++ active.go
@@ -2,17 +2,27 @@ package hyparview
 
 type ActiveView struct {
 	Nodes []Node
-	MAX   int
+	Max   int
 }
 
 func (v *ActiveView) IsFull() bool {
-	return len(v.Nodes) >= v.MAX
+	return len(v.Nodes) >= v.Max
 }
 
 func (v *ActiveView) Add(n Node) {
 	if !v.Contains(n) {
 		v.Nodes = append(v.Nodes, n)
 	}
+}
+
+func (v *ActiveView) DelIndex(i int) {
+	ns := v.Nodes
+	mx := len(ns) - 1
+	v.Nodes = append(ns[0:i], ns[i+1:mx]...)
+}
+
+func (v *ActiveView) GetIndex(i int) Node {
+	return v.Nodes[i]
 }
 
 func (v *ActiveView) Contains(n Node) bool {
