d0a38f07683a69947d6438e5f1317f9456d9cdb5
Lang Martin <lang@hashicorp.com>
2019-11-18 19:24:11 -0500

    util compile time configure rint

--- util.go
+++ util.go
@@ -6,15 +6,15 @@ import (
 	"math/rand"
 )
 
+func rintCrypto(n int) int {
+	bn := new(big.Int).SetInt64(int64(n))
+	bi, _ := cr.Int(cr.Reader, bn)
+	i := int(bi.Int64())
+	return i
+}
+
 // rint returns a random integer, but allows to choose at configure time to use the pseudo
 // random or cryptographic random number generator
-func (v *Hyparview) rint(n int) int {
-	if v.CryptoRand {
-		bn := new(big.Int).SetInt64(int64(n))
-		bi, _ := cr.Int(cr.Reader, bn)
-		i := int(bi.Int64())
-		return i
-	}
-	// math/rand
+func rint(n int) int {
 	return rand.Intn(n)
 }
