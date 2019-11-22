4c25cbfa352e41511f64704ecae739bf47bdbb6b
Lang Martin <lang@hashicorp.com>
2019-11-21 22:25:01 -0500

    client test uses the new stuff

--- client_test.go
+++ client_test.go
@@ -6,18 +6,27 @@ type Client struct {
 	out []Message
 }
 
+func (c *Client) create(id string, addr string, n *Node) *Hyparview {
+	v := CreateView(&Node{ID: id, Addr: addr}, 0)
+	ms := n.v.RecvJoin(SendJoin(n, v.Self))
+	c.out = append(c.out, ms...)
+	return v
+}
+
 func (c *Client) failActive(failed *Node) {
 	var node *Node
 	for _, n := range c.v.Passive.Shuffled() {
 		if v.Active.IsEmpty() {
 			// simulate sync network call
 			// TODO simulate failure
-			ms := n.v.RecvNeighbor(HighPriority, c.v.Self)
+			m := SendNeighbor(n, c.v.Self, HighPriority)
+			ms := n.v.RecvNeighbor(m)
 			// forward maybe disconnect messages
 			c.out = append(c.out, ms)
 			break
 		} else {
-			ms := n.v.RecvNeighbor(LowPriority, c.v.Self)
+			m := SendNeighbor(n, c.v.Self, LowPriority)
+			ms := n.v.RecvNeighbor(m)
 			// any low priority response is failure
 			if len(ms) == 0 {
 				c.v.DelPassive(n)
