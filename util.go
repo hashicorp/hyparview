854f6f43002eb3d7f8b22335c6225febef29f797
Lang Martin <lang@hashicorp.com>
2019-11-23 12:25:43 -0500

    util: shuffle wants rint to be inclusive

--- util.go
+++ util.go
@@ -6,14 +6,16 @@ import (
 	"math/rand"
 )
 
+// rand [0, n] inclusive
 func rintCrypto(n int) int {
-	bn := new(big.Int).SetInt64(int64(n))
+	bn := new(big.Int).SetInt64(int64(n + 1))
 	bi, _ := cr.Int(cr.Reader, bn)
 	i := int(bi.Int64())
 	return i
 }
 
 // rint is a placeholder so we can swap out for rintCrypto in testing
+// rand [0, n] inclusive
 func rint(n int) int {
-	return rand.Intn(n)
+	return rand.Intn(n + 1)
 }
