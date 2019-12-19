656bdcbe862974852fe8c91ae27e2091c83c4826
Lang Martin <lang@hashicorp.com>
2019-12-18 20:56:43 -0500

    util Rint64Crypto, need an int64 for seed

--- util.go
+++ util.go
@@ -7,11 +7,15 @@ import (
 )
 
 // rand [0, n] inclusive
-func rintCrypto(n int) int {
+func Rint64Crypto(n int64) int64 {
 	bn := new(big.Int).SetInt64(int64(n + 1))
 	bi, _ := cr.Int(cr.Reader, bn)
-	i := int(bi.Int64())
-	return i
+	return bi.Int64()
+}
+
+// rand [0, n] inclusive
+func RintCrypto(n int) int {
+	return int(Rint64Crypto(int64(n)))
 }
 
 // rint is a placeholder so we can swap out for rintCrypto in testing
