e92fda833f0485639e592169f03b5fa61a37fa48
Lang Martin <lang@hashicorp.com>
2020-03-07 15:09:19 -0500

    message: use genny to wrap getters around the requests

--- message.go
+++ message.go
@@ -1,141 +1,21 @@
+//go:generate genny -in=message.go -out=message-gen.go gen "genericReq=JoinRequest,ForwardJoinRequest,DisconnectRequest,NeighborRequest,NeighborRefuse,ShuffleRequest,ShuffleReply,Gossip"
 package hyparview
+
+import "github.com/cheekybits/genny/generic"
 
 type Message interface {
 	To() *Node
+	AssocTo(*Node) Message
 }
 
-const (
-	HighPriority = true
-	LowPriority  = false
-)
+type genericReq generic.Type
 
-type JoinRequest struct {
-	to   *Node
-	From *Node
+func (r *genericReq) To() *Node {
+	return r.to
 }
 
-func (r *JoinRequest) To() *Node { return r.to }
-
-func SendJoin(to *Node, from *Node) *JoinRequest {
-	return &JoinRequest{
-		to:   to,
-		From: from,
-	}
-}
-
-type ForwardJoinRequest struct {
-	to   *Node
-	From *Node
-	Join *Node
-	TTL  int
-}
-
-func (r *ForwardJoinRequest) To() *Node { return r.to }
-
-func SendForwardJoin(to *Node, from *Node, join *Node, ttl int) *ForwardJoinRequest {
-	return &ForwardJoinRequest{
-		to:   to,
-		From: from,
-		Join: join,
-		TTL:  ttl,
-	}
-}
-
-type DisconnectRequest struct {
-	to   *Node
-	From *Node
-}
-
-func (r *DisconnectRequest) To() *Node { return r.to }
-
-func SendDisconnect(to *Node, from *Node) *DisconnectRequest {
-	return &DisconnectRequest{
-		to:   to,
-		From: from,
-	}
-}
-
-type NeighborRequest struct {
-	to       *Node
-	From     *Node
-	Priority bool
-	Join     bool
-}
-
-func (r *NeighborRequest) To() *Node { return r.to }
-
-func SendNeighbor(to *Node, from *Node, priority bool) *NeighborRequest {
-	return &NeighborRequest{
-		to:       to,
-		From:     from,
-		Priority: priority,
-	}
-}
-
-func SendNeighborJoin(to *Node, from *Node) *NeighborRequest {
-	return &NeighborRequest{
-		to:       to,
-		From:     from,
-		Priority: HighPriority,
-		Join:     true,
-	}
-}
-
-type NeighborRefuse struct {
-	to   *Node
-	From *Node
-}
-
-func (r *NeighborRefuse) To() *Node { return r.to }
-
-func SendNeighborRefuse(to *Node, from *Node) *NeighborRefuse {
-	return &NeighborRefuse{
-		to:   to,
-		From: from,
-	}
-}
-
-type ShuffleRequest struct {
-	to      *Node
-	From    *Node
-	Origin  *Node
-	Active  []*Node
-	Passive []*Node
-	TTL     int
-}
-
-func (m *ShuffleRequest) To() *Node { return m.to }
-
-func SendShuffle(to, from, origin *Node, active, passive []*Node, ttl int) *ShuffleRequest {
-	return &ShuffleRequest{
-		to:      to,
-		From:    from,
-		Origin:  from,
-		Active:  active,
-		Passive: passive,
-		TTL:     ttl,
-	}
-}
-
-type ShuffleReply struct {
-	to      *Node
-	From    *Node
-	Passive []*Node
-}
-
-func (m *ShuffleReply) To() *Node { return m.to }
-
-func SendShuffleReply(to *Node, from *Node, passive []*Node) *ShuffleReply {
-	return &ShuffleReply{
-		to:      to,
-		From:    from,
-		Passive: passive,
-	}
-}
-
-type Gossip struct {
-	to      *Node
-	From    *Node
-	Payload int
-	Hops    int
+func (r *genericReq) AssocTo(n *Node) *Node {
+	o := *r
+	o.to = n
+	return &o
 }
