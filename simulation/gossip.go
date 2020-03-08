e440afc2ad145a3d820b3162ef2e40d36db297c3
Lang Martin <lang@hashicorp.com>
2020-03-08 16:21:06 -0400

    simulation/gossip: use the Message interface, gossip method

--- simulation/gossip.go
+++ simulation/gossip.go
@@ -9,55 +9,35 @@ type gossip struct {
 	hops int
 }
 
-func newGossip(to, from *h.Node, payload, hops int) *gossip {
-	return &gossip{
-		to:   to,
-		from: from,
-		app:  payload,
-		hops: hops,
-	}
+// Implement the message interface for gossip
+func (r *gossip) To() *h.Node {
+	return r.to
 }
 
-func (c *Client) gossip(i int) []*gossip {
-	return c.recvGossip(&gossip{to: c.Self, app: i, hops: 0})
+func (r *gossip) AssocTo(n *h.Node) h.Message {
+	o := *r
+	o.to = n
+	return &o
+}
+
+func (r *gossip) From() *h.Node {
+	return r.from
+}
+
+func (c *Client) gossip(i int) {
+	c.recvGossip(&gossip{to: c.Self, app: i, hops: 0})
 }
 
 // Example gossip implementation. For deterministic testing, each payload runs until the
 // message is completely distributed.
-func (c *Client) recvGossip(m *gossip) (ms []*gossip) {
+func (c *Client) recvGossip(m *gossip) {
 	if c.app >= m.app {
 		c.appWaste += 1
-		return ms
+		return
 	}
 	c.app = m.app
 	c.appHops = m.hops
 	c.appSeen += 1
 
-	for _, n := range c.Active.Nodes {
-		if n.Equal(m.from) {
-			continue
-		}
-
-		peer := c.world.get(n.ID)
-		// FIXME retry with the fixed connection
-		if c.world.shouldFail() {
-			c.world.send(c.failActive(peer)...)
-		}
-
-		ms = append(ms, newGossip(n, c.Self, m.app, m.hops+1))
-	}
-
-	return ms
-}
-
-// Send the gossip messages and all messages caused by them. Just like world.send, except
-// for accounting; we want to keep counts separately to properly manage the graphs. FIXME
-// integrate this sensibly
-func (w *World) sendGossip(ms ...*gossip) {
-	for _, m := range ms {
-		v := w.get(m.to.ID)
-		if v != nil {
-			w.sendGossip(v.recvGossip(m)...)
-		}
-	}
+	c.Gossip(&gossip{from: c.Self, app: m.app, hops: m.hops + 1})
 }
