65b3daa32b312312657663c817f4ae85d3d00ed9
Lang Martin <lang@hashicorp.com>
2020-03-08 14:45:21 -0400

    failure: new file wrapping send and failure recovery

---
+++ failure.go
@@ -0,0 +1,67 @@
+package hyparview
+
+type Send interface {
+	// Send sends one message at a time to a peer. TODO simplify batching?
+	// send should use a timeout to detect blocking as failure
+	Send(Message) (Message, error)
+	// Failed is called after hyparview has handled the failure, to handle e.g.
+	// connection cleanup
+	Failed(*Node)
+}
+
+// Send wraps the S.Send sender in appropriate error handling
+func (v *Hyparview) Send(ms ...Message) {
+	subs := map[string]*Node{}
+
+	for i := 0; i < len(ms); {
+		m := ms[i]
+		n := m.To()
+
+		// Send messages to the replacement node, if we have one
+		if sub, ok := subs[n.ID]; ok {
+			m = m.AssocTo(sub)
+		}
+
+		_, err := v.S.Send(m)
+		if err != nil {
+			v.Active.DelNode(n)
+			sub := v.PromotePassive(n)
+			if sub == nil {
+				// FIXME ?
+			}
+			subs[n.ID] = sub
+			v.S.Failed(n)
+		} else {
+			// On failure, retry the failed message with the replacement server
+			i++
+		}
+	}
+}
+
+func (v *Hyparview) PromotePassive() *Node {
+	for _, n := range v.Passive.Shuffled() {
+		pri := v.Active.IsEmpty()
+		m := SendNeighbor(n, c.Self, pri)
+
+		resp, err := v.S.Send(m)
+		if err != nil {
+			v.Passive.DelNode(n)
+			continue
+		}
+
+		if pri == h.HighPriority {
+			c.AddActive(n)
+			c.DelPassive(n)
+			return n
+		}
+
+		// Low priority, a refuse means we move on but keep the peer
+		if len(resp) != 0 {
+			continue
+		}
+
+		c.AddActive(n)
+		c.DelPassive(n)
+		return n
+	}
+}
