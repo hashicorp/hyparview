533d2f809906ea7e4e24672a9d942c60e4331789
Lang Martin <lang@hashicorp.com>
2020-03-09 09:29:16 -0400

    simulation/client: keep each client's message history

--- simulation/client.go
+++ simulation/client.go
@@ -1,16 +1,19 @@
 package simulation
 
 import (
+	"fmt"
+
 	h "github.com/hashicorp/hyparview"
 )
 
 type Client struct {
 	h.Hyparview
 	w        *World
-	app      int // final value we got
-	appHops  int // final value's hops
-	appSeen  int // if app == appSeen, we got every message
-	appWaste int // count of app messages that didn't change the value
+	history  []h.Message // debug history of messages
+	app      int         // final value we got
+	appHops  int         // final value's hops
+	appSeen  int         // if app == appSeen, we got every message
+	appWaste int         // count of app messages that didn't change the value
 }
 
 func makeClient(w *World, id string) *Client {
@@ -30,15 +33,26 @@ func (c *Client) recv(m h.Message) h.Message {
 		c.recvGossip(m1)
 		return nil
 	default:
+		c.w.totalMessages += 1
+		c.history = append(c.history, m)
 		return c.Recv(m)
 	}
 }
 
 // Implement the sender interface
 func (c *Client) Send(m h.Message) (h.Message, error) {
-	c.w.totalMessages += 1
+	if h.Rint(100) < c.w.config.failureRate {
+		return nil, fmt.Errorf("random error")
+	}
+
+	c.history = append(c.history, m)
 	peer := c.w.get(m.To().ID)
-	return peer.recv(m), nil
+	o := peer.recv(m)
+	if o != nil {
+		c.history = append(c.history, o)
+	}
+
+	return o, nil
 }
 
 func (c *Client) Failed(peer *h.Node) {
