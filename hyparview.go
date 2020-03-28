50200cf49884a4e879b60a541c6cd68368d32c9a
Lang Martin <lang@hashicorp.com>
2020-03-28 13:20:27 -0400

    hyparview: don't forward join to either the sender or subject

--- hyparview.go
+++ hyparview.go
@@ -84,7 +84,7 @@ func (v *Hyparview) RecvForwardJoin(r *ForwardJoinRequest) {
 
 	// Forward to one not-sender active peer
 	for _, n := range v.Active.Shuffled() {
-		if n.Equal(r.From()) {
+		if n.Equal(r.From()) || n.Equal(r.Join) {
 			continue
 		}
 		v.Send(NewForwardJoin(n, v.Self, r.Join, r.TTL-1))
